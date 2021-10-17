0196ed3e5ebe1f99af6a3e8b27fe9d9b85c91ff7
Sushaan Maharjan <sushaanmaharjan1@gmail.com>
2021-10-17 13:36:41 +0545

    POST GET and Search by id

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
+require('dotenv').config()
+const bodyParser = require('body-parser')
 const express=require("express")
+const bodyparser=require("body-parser")
 const app=express()
 
-app.get("/",(request,response) =>{
-    response.json({
-        message:"Hello World"
-    })
-});
+let users=[] //id, name, email, address
+
+app.use(bodyparser.json())
 
 
-app.get("/profile",(request,response) =>{
-    response.send("This is a Profile page")
-});
 
-app.listen(3000)
+//crud
+
+//get new user Request method:Get
+app.get('/users',(req,res)=>{
+    res.json(users)
+})
+
+//create new user Request method:Post
+
+app.post('/users',(req,res)=>{
+    users.push(req.body)
+    res.status(201).json(req.body)
+})
+
+//get user by id Request method:Get
+app.get('/users/:id',(req,res)=>{
+    let user=users.find(user=>user.id === parseInt(req.params.id))
+    if (!user) res.status(404).send("The user with the given ID was not found.")
+    res.json(user)
+})
+
+console.log(process.env.DB_Host,"host")
+const port =process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log(`Server is running on ${port}`)
+})
+
+
+
+
+
+
+
+
+// app.get('/',(req,res)=>{
+//     res.json({
+//         message:"Hello world"
+//     })
+// })
+// app.get("/profile",(req,res)=>{
+//     res.send("This is my profile")
+// })
